2dad98845352d970713417ef8a45a2b8dabc2ef2
throw-platform <joecohen@ieee.org>
2013-05-22 21:53:18 -0400

    added more to showcase

--- showcase/thumbnail.sh
+++ showcase/thumbnail.sh
@@ -1,2 +1,8 @@
-rm *-thumb.png
-for i in *; do convert -thumbnail 400x $i $i-thumb.png; done
+rm *thumb.png
+for i in *; do 
+convert -thumbnail 400x $i $i-scaled-thumb.png
+
+convert $i-scaled-thumb.png  -virtual-pixel white -set option:distort:viewport "400x260-0-0" \
+     -gravity center -filter point -distort SRT 0 +repage $i-thumb.png; 
+rm $i-scaled-thumb.png
+done
